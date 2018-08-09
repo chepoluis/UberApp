3132bc04e2753829b7895e53d6ef508c39e263c5
Luis <vsjoseluisvs@gmail.com>
2018-08-08 20:45:04 -0500

    Video 29

--- app/src/main/java/firebase/database/insert/ejemplo/com/uber/historyRecyclerView/HistoryViewHolders.java
+++ app/src/main/java/firebase/database/insert/ejemplo/com/uber/historyRecyclerView/HistoryViewHolders.java
@@ -1,9 +1,12 @@
 package firebase.database.insert.ejemplo.com.uber.historyRecyclerView;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import firebase.database.insert.ejemplo.com.uber.HistorySingleActivity;
 import firebase.database.insert.ejemplo.com.uber.R;
 
 public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
@@ -19,8 +22,13 @@ public class HistoryViewHolders extends RecyclerView.ViewHolder implements View.
         time = (TextView) itemView.findViewById(R.id.time);
     }
 
+    // Is triggered when a card of recycler view is click
     @Override
     public void onClick(View v) {
-
+        Intent intent = new Intent(v.getContext(), HistorySingleActivity.class);
+        Bundle b = new Bundle();
+        b.putString("rideId", rideId.getText().toString());
+        intent.putExtras(b);
+        v.getContext().startActivity(intent);
     }
 }
